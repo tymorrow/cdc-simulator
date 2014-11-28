4389d80f7849d395fcc91f0406b621d4be6e307d
Ty Morrow <tjm6f4@mst.edu>
2014-11-28 00:34:49 -0600

    Add second instruction set.

--- CdcSimulator/Program.cs
+++ CdcSimulator/Program.cs
@@ -8,16 +8,18 @@
         static void Main(string[] args)
         {
             var set1 = new InstructionSet();
-            var set2 = new InstructionSet();
-
             // Create the CDC6600 machine object and pass it the instructions to run
             var cdc6600 = new Cdc6600();
-            cdc6600.AddInstructions(set2.InstructionSet1);
+            cdc6600.AddInstructions(set1.InstructionSet1);
+            cdc6600.Run();
+            cdc6600 = new Cdc6600();
+            cdc6600.AddInstructions(set1.InstructionSet2);
             cdc6600.Run();
 
+            var set2 = new InstructionSet();
             // Create the CDC7600 machine object and pass it the instructions to run
             var cdc7600 = new Cdc7600();
-            cdc7600.AddInstructions(set1.InstructionSet1);
+            cdc7600.AddInstructions(set2.InstructionSet1);
             //cdc7600.Run();
 
             Console.ReadKey();
